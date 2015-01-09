effa0cd979de22bc1f3dfef93faf90b7720f1a85
zxysnow <zxy_snow@qq.com>
2015-01-09 13:39:32 +0800

    Length of Last Word

--- 58-length-of-last-word.cpp
+++ 58-length-of-last-word.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int lengthOfLastWord(const char *s) {
-        int ans = 0;
+        if( *s == '\0' )
+            return 0;
+        int ans = (*s++ == ' ' ? 0 : 1);
         while (*s != '\0') {
             if (*s != ' ' && *(s-1) == ' ') 
                 ans = 0;
