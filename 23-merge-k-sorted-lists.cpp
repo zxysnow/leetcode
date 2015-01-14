03ce1095eb992955a8788ab2e056786e8de09b7e
zxysnow <zxy_snow@qq.com>
2015-01-14 16:58:34 +0800

    Merge k Sorted Lists-O(n*log(n))

---
+++ 23-merge-k-sorted-lists.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    struct cmp{ 
+        bool operator()(const ListNode* a, const ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> q;
+        ListNode ans = ListNode(0);
+        ListNode* ahead = &ans;
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i])
+                q.push(lists[i]);
+        }
+        while (!q.empty()){
+            ListNode* head = q.top();
+            q.pop();
+            ahead->next = head;
+            if (head->next != NULL)
+                q.push(head->next);
+            ahead = ahead->next;
+            ahead->next = NULL;
+        }
+        return ans.next;
+    }
+};
