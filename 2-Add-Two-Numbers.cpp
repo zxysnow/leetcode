1f7e22531a92a0933d54ce889e582d45e7292703
zxysnow <zxy_snow@qq.com>
2015-07-22 16:01:54 +0800

    update Add Two Numbers

--- 2-Add-Two-Numbers.cpp
+++ 2-Add-Two-Numbers.cpp
@@ -9,31 +9,23 @@
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode ans(0);
+        ListNode *sum = &ans;
         int carry = 0;
-        ListNode *ans = new ListNode(0);
-        ListNode *head = ans;
-
-        while( l1 || l2 || carry ) {
-            int num = -1;
-            if( l1 ) {
-                num = l1 -> val;
-                l1 = l1 -> next;
+        while (l1 || l2 || carry) {
+            int s = carry;
+            if (l1) {
+                s += l1->val;
+                l1 = l1->next;
             }
-            if( l2 ) {
-                if( num == -1 )
-                    num = 0;
-                num += l2 -> val;
-                l2 = l2 -> next;
+            if (l2) {
+                s += l2->val;
+                l2 = l2->next;
             }
-            if( num == -1 ) {
-                ans -> next = new ListNode(carry);
-                carry = 0;
-            } else {
-                ans -> next = new ListNode((num + carry) % 10);
-                carry = (num + carry) / 10;
-            }
-            ans = ans -> next;
+            carry = s / 10;
+            sum->next = new ListNode(s % 10);
+            sum = sum->next;
         }
-        return head -> next;
+        return ans.next;
     }
 };
