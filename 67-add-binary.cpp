f51bfab53a7769717f64eacf17ad135a5d31a640
zxysnow <zxy_snow@qq.com>
2015-07-31 11:32:20 +0800

    update

--- 67-add-binary.cpp
+++ 67-add-binary.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    string tail(string b, int n, int &flag) {
-        string sum = "";
-        while ((int)b.size() - n - 1 >= 0) {
-            int s = b[(int)b.size() - n - 1] - '0' + flag;
-            flag = s / 2;
-            s %= 2;
-            sum += (s + '0');
-            n++;
-        }
-        return sum;
-    }
     string addBinary(string a, string b) {
-        string sum = "";
-        int n = min(a.size(), b.size());
-        int an = a.size(), bn = b.size();
-        int flag = 0;
-        for (int i = 0; i < n; i++) {
-            int s = a[an - i - 1] - '0' + b[bn - i - 1] - '0' + flag;
-            flag = s / 2;
-            s = s % 2;
-            sum += (s + '0');
+        ostringstream oss;
+        int  i = a.size() - 1, k = b.size() - 1;
+        int carry = 0;
+        while (i >= 0 || k >= 0) {
+            int num = carry;
+            if (i >= 0)
+                num += a[i--] - '0';
+            if (k >= 0)
+                num += b[k--] - '0';
+            carry = num / 2;
+            num %= 2;
+            oss << char(num + '0');
         }
-        sum += tail(a, n, flag);
-        sum += tail(b, n, flag);
-        if (flag)
-            sum += '1';
-        reverse(sum.begin(), sum.end());
-        return sum;
+        if (carry)
+            oss << '1';
+        string ans = oss.str();
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
