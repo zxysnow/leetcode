a5749c2d94cc0fe8b787bd9abc919bb4642d0ef2
zxy_snow <zxy_snow@qq.com>
2015-08-04 20:26:51 +0800

    update

--- 38-count-and-say.cpp
+++ 38-count-and-say.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     string countAndSay(int n) {
         string s = "1";
-        while (--n) {
-            string t = "";
-            int cnt = 1;
-            s += '#';
-            for (int i = 1; i < s.size(); i++) {
-                if (i < s.size() && s[i] == s[i - 1]) {
-                    while (i < s.size() && s[i] == s[i - 1]) {
-                        i++;
-                        cnt++;
-                    }
-                }
-                t += char('0' + cnt);
-                t += s[i - 1];
-                cnt = 1;
-            }
-            s = t;
-        }
+        for (int i = 0; i < n - 1; i++)
+            s = say(s);
         return s;
+    }
+
+private :
+    string say(string s) {
+        ostringstream oss;
+        int cnt = 1, n = s.size();
+        for (int i = 1; i <= n; i++) {
+            if (i == n || s[i] != s[i - 1]) {
+                oss << cnt << s[i - 1];
+                cnt = 1;
+            } else {
+                ++cnt;
+            }
+        }
+        return oss.str();
     }
 };
