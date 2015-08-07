b318c3f0e780ac9f6e4ba7198f8196fe6f7b889c
zxysnow <zxy_snow@qq.com>
2015-08-07 10:04:19 +0800

    update

--- 77-combinations.cpp
+++ 77-combinations.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     vector<vector<int> > combine(int n, int k) {
         vector<vector<int> > ans;
-        vector<int> v;
-        dfs(0, n, k, v, ans);
+        if (k > n)
+            return ans;
+        vector<int> nums;
+        dfs(1, k, n, ans, nums);
         return ans;
     }
-
+    
 private :
-    void dfs(int x, int n, int cnt, vector<int> &v, vector<vector<int> > &ans) {
-        if (cnt == 0) {
-            ans.push_back(v);
+    void dfs(int x, int k, int n, vector<vector<int> > &ans, vector<int> &nums) {
+        if (nums.size() == k) {
+            ans.push_back(nums);
             return ;
         }
-        for (int i = x; i < n; i++) {
-            v.push_back(i + 1);
-            dfs(i + 1, n, cnt - 1, v, ans);
-            v.pop_back();
+        for (int i = x; i <= n; i++) {
+            if (nums.size() + n - i + 1 < k)
+                return ;
+            nums.push_back(i);
+            dfs(i + 1, k, n, ans, nums);
+            nums.pop_back();
         }
     }
 };
