9b96fd86347fa47f5819da70f5f124307c96d7c8
zxysnow <zxy_snow@qq.com>
2015-08-14 15:45:21 +0800

    update

--- 52-n-queens-ii.cpp
+++ 52-n-queens-ii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int totalNQueens(int n) {
-       int ans = 0;
-       dfs(0, n, ans);
-       return ans;
+        int ans = 0;
+        int column = 0, ld = 0, rd = 0;
+        dfs(0, column, ld, rd, ans, n);
+        return ans;
     }
-    
+
 private :
-    vector<pair<int, int> > Q;
-    bool place(int x, int i) {
-        for (int k = 0; k < Q.size(); k++)
-            if (x == Q[k].first || i == Q[k].second || abs(Q[k].first - x) == abs(Q[k].second - i))
-                return false;
-        return true;
-    }
-    void dfs(int x, int n, int &ans) {
+    void dfs(int x, int column, int ld, int rd, int &ans, int n) {
         if (x == n) {
-            ans++;
+            ++ans;
             return ;
         }
         for (int i = 0; i < n; i++) {
-            if (place(x, i)) {
-                Q.push_back(make_pair(x, i));
-                dfs(x + 1, n, ans);
-                Q.pop_back();
+            int id = x + i;
+            int id2 = n - x - 1 + i;
+            if ((column & (1 << i)) == 0 && (ld & (1 << id)) == 0 && (rd & (1 << id2)) == 0) {
+                dfs(x + 1, column | (1 << i), ld | (1 << id), rd | (1 << id2), ans, n);
             }
         }
     }
 };
+
